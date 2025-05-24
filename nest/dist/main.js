42b40edb3f48492ad954d0b8dd010777ed6362c7
Karen <karen.gabrielyan93@gmail.com>
2025-05-24 15:34:29 +0400

    Added php server

---
+++ nest/dist/main.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('BringIt')
+        .setDescription('The API description')
+        .setVersion('1.0')
+        .addTag('BringIt')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    await app.listen(3000);
+    console.log('Server started at port u');
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
